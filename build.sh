f41df28e479d73a150fb7b6d6d9e5b5a19c8c9b2
appseed-projects6 <133564452+appseed-projects6@users.noreply.github.com>
2023-05-14 11:27:43 +0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+#__API_GENERATOR__
+python manage.py generate-api -f
+#__API_GENERATOR__END
